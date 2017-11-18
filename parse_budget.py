2416a9db4e6f27bd792ab2446bfcf33feac49bac
csillafulep <fulepcsilla@gmail.com>
2017-11-18 20:03:52 +0100

    budget

--- parse_budget.py
+++ parse_budget.py
@@ -2,8 +2,20 @@ import urllib.request  as urllib2
 from bs4 import BeautifulSoup
 from utils import *
 
-def parse_budget(xmlFile):
-    for h4 in xmlFile.find_all('h4'):
-        if "Budget:" in h4:
-            budget = h4.next_sibling.strip()
-    return budget
+def parse_budget(xmlFile, id):
+    try:
+        for h4 in xmlFile.find_all('h4'):
+            if "Budget:" in h4:
+                budgetStr = h4.next_sibling.strip()
+                if budgetStr[0] == '$':
+                    budget = int(budgetStr[1:].replace(',',''))
+                    return budget
+                else:
+                    return None
+        
+        return None
+       
+    
+    except:
+        print('Could parse budget for id ' + id)
+        return None
