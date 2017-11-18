97304f21a80d79a6c52c1212e56980312afe8202
csillafulep <fulepcsilla@gmail.com>
2017-11-18 11:32:10 +0100

    read ID from Kaggle file

---
+++ readIDfromKaggle.py
@@ -0,0 +1,15 @@
+import urllib.request  as urllib2 
+from bs4 import BeautifulSoup
+import csv
+
+def readIdsFromKaggle():
+    IDs=[]
+
+
+    with open('imdb2.csv') as f:
+        lines = f.readlines()
+        for i in range(1, len(lines)):
+            line = lines[i]
+            words = line.split(',')
+            IDs.append(words[1])
+    return IDs
